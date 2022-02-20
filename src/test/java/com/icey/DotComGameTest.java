2a645120f5cfd7bbfeb25e95f59c7128d3493a40
ICEY-Pro <1539901457@qq.com>
2022-02-20 17:31:32 +0800

    DotComGame

---
+++ src/test/java/com/icey/DotComGameTest.java
@@ -0,0 +1,18 @@
+package com.icey;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class DotComGameTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
